27e019e099dd49e78b258cebb9b2c7727ab8b2e6
Prajjval-Tomar <prajjavaltomar920@gmail.com>
2026-03-29 22:55:20 +0530

    feat: added production settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -120,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]  # later restrict in AWS
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
